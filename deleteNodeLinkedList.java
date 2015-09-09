79993b4ab67a8fcd5a0bdbde12334e76638d7cee
harsham05 <hmanjuna@usc.edu>
2015-09-08 22:09:32 -0700

    update, java formality

--- deleteNodeLinkedList.java
+++ deleteNodeLinkedList.java
@@ -1,7 +1,17 @@
 
-public static void del(Node node)
+public class deleteNodeLinkedList
 {
-    node.val = node.next.val;
-    node.next = node.next.next
-    //empty reference cleaned up by Garbage Collector
+
+    public static void del(Node node)
+    {
+        node.val = node.next.val;
+        node.next = node.next.next
+        //empty reference cleaned up by Garbage Collector
+    }
+
+    public static void main(String[] args)
+    {
+        del(node);
+    }
+    
 }
